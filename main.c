f1b786ba3aa626cbbb12db2246de983db5d9d941
Erick Avila <yahir7083@gmail.com>
2022-09-05 20:40:23 -0500

    INit

---
+++ main.c
@@ -0,0 +1,62 @@
+// Librerías
+#include <stdio.h>
+#include <stdlib.h>
+
+// Función principal
+int main() {
+  // Ajuste de color
+  system("color 1F");
+
+  // Encabezado con marco
+  printf("%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\n", 175, 175, 175, 175, 175, 175,
+         175, 175, 175, 175, 175, 175, 175, 175, 175, 175);
+  printf("%c Hoyos Negros %c\n", 175, 175);
+  printf("%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\n", 175, 175, 175, 175, 175, 175,
+         175, 175, 175, 175, 175, 175, 175, 175, 175, 175);
+
+  // Autor del actículo
+  printf("Autor: Consuelo Doddoli, Ciencia UNAM-DGDC.\n\n\n");
+
+  // Redacción
+  printf("Existen distintos tipos de hoyos negros. Unos de los m%cs conocidos "
+         "son los que se forman cuando las estrellas que contienen por lo "
+         "menos 20 veces la masa del Sol agotan el combustible de su n%ccleo y "
+         "colapsan, formando un agujero negro.\n\n\n",
+         160, 163);
+
+  printf("Otros son los llamados supermasivos, que contienen entre cientos de "
+         "miles a miles de millones de masas solares. Se encuentran en los "
+         "centros de las galaxias masivas, como la nuestra, explica la doctora "
+         "Julieta Fierro, investigadora del Instituto de Astronom%ca de la "
+         "UNAM.\n\n\n",
+         162);
+
+  printf("Aclara que los hoyos negros supermasivos se forman por la fusi%cn de "
+         "otros hoyos negros, estrellas y gas, ya que en los n%ccleos de las "
+         "galaxias masivas existe una mayor densidad de materia, que en el "
+         "plano de la galaxia, d%cnde est%cn los brazos.\n\n\n",
+         162, 163, 162, 160);
+
+  printf("Fue hasta 2019 que un grupo de astr%cnomos logr%c por primera vez la "
+         "fotograf%ca del material de la orilla del horizonte de eventos de un "
+         "hoyo negro. Este se ubica en el centro de la enorme galaxia M87 que "
+         "se encuentra a 60 millones de años luz de nosotros.\n\n\n",
+         162, 162, 161);
+
+  // Bibliografía
+  printf("Bibliograf%ca\n", 161);
+  printf("Consuelo Doddoli. %c2022%c. Hoyos Negros %cPor qu%c es importante la "
+         "imagen de Sagitario A%c?. Septiembre 5, 2022, de Divulgaci%cn de la "
+         "Ciencia, UNAM Sitio web%c https%c%cbit.ly%c3Rd6ytz\n\n\n",
+         40, 41, 168, 130, 42, 162, 58, 47, 47, 47);
+
+  // Cambio de color
+  system("pause");    // Pausa en flujo de ejecución
+  system("cls");      // Limpiar pantalla
+  system("color 2F"); // Asignar color
+
+  // Mensaje
+  printf("Programa 5 creado por: %cAvila Barba Erick Yahir%c\n", 40, 41);
+
+  return 0;
+}
